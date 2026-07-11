cb0945a3812072d941801fc065190c47858c19fc
Ali <ali2moh.04@gmail.com>
2026-07-11 19:57:06 +0300

    Add serializers

---
+++ backend/core_data/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Country, Indicator, DataPoint
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ['id', 'name', 'iso2_code', 'iso3_code', 'region', 'income_level']
+
+class IndicatorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Indicator
+        fields = ['id', 'code', 'name', 'description', 'category', 'unit']
+
+class DataPointSerializer(serializers.ModelSerializer):
+    country = CountrySerializer(read_only=True)
+    indicator = IndicatorSerializer(read_only=True)
+
+    class Meta:
+        model = DataPoint
+        fields = ['id', 'country', 'indicator', 'year', 'value']
