0e100fa1de25713ccb248e66c020cb76fdc14da8
Ali <ali2moh.04@gmail.com>
2026-07-11 19:48:37 +0300

    Registered Models

--- backend/core_data/admin.py
+++ backend/core_data/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Country, Indicator, DataPoint
 
-# Register your models here.
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'iso2_code', 'iso3_code', 'region', 'income_level')
+    search_fields = ('name', 'iso2_code', 'iso3_code')
+    list_filter = ('region', 'income_level')
+
+@admin.register(Indicator)
+class IndicatorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'category', 'description')
+    search_fields = ('name', 'code', 'category', 'description')
+    list_filter = ('category',)
+
+@admin.register(DataPoint)
+class DataPointAdmin(admin.ModelAdmin):
+    list_display = ('country', 'indicator', 'year', 'value')
+    search_fields = ('country__name', 'country__iso3_code', 'indicator__name', 'indicator__code')
+    list_filter = ('year', 'country', 'indicator')
